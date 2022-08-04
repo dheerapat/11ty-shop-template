8cd29e77ac9a50ef63d4322a2675cee0c7e2f33f
dheerapat <dheerapat.t@kkumail.com>
2022-08-04 15:50:30 +0700

    11ty shop template

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/css/')
+    eleventyConfig.addPassthroughCopy('./src/js/')
+    eleventyConfig.addPassthroughCopy('./src/img/')
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    }
+}
